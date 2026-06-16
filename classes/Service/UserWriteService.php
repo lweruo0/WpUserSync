c49f183c2b30a683a8a40223544aec97de54b25f
lweruo0 <Oliver.Ruoss@liebherr.com>
2026-06-16 10:15:20 +0200

    ddd

--- classes/Service/UserWriteService.php
+++ classes/Service/UserWriteService.php
@@ -265,7 +265,7 @@ final class UserWriteService
         return [
             'status' => 'success',
             'message' => 'User created successfully.',
-            'userId' => $user->getId(),
+            'userId' => $usr_id = (int) $user->getValue('usr_id');  
         ];
     }
 
